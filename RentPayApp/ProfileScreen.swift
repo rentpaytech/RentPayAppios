c4a0ed9e4b80591896108d4a7cb82b3f9ad9f56b
rentpaytech <131189156+rentpaytech@users.noreply.github.com>
2023-07-26 11:09:27 +0530

    Create ProfileScreen.swift

---
+++ RentPayApp/ProfileScreen.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileScreen.swift
+//  RentPayApp
+//
+//  Created by Ritik Yadav on 31/05/23.
+//
+
+import UIKit
+
+class ProfileScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
