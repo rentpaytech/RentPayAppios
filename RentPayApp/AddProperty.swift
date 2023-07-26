28af5ba10657123dfec18fcddd4e8d436d038418
rentpaytech <131189156+rentpaytech@users.noreply.github.com>
2023-07-26 10:55:57 +0530

    Create AddProperty.swift

---
+++ RentPayApp/AddProperty.swift
@@ -0,0 +1,18 @@
+//
+//  AddProperty.swift
+//  RentPayApp
+//
+//  Created by yadav, Ritik on 12/05/23.
+//
+
+import UIKit
+
+class AddProperty: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
